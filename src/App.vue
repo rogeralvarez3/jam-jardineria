30d612d2b687d9bb4240497ed699a052373e55a3
rogeralvarezes <rogeralvarezes@gmail.com>
2026-09-09 21:59:37 +0200

    agregué cursor animado

--- src/App.vue
+++ src/App.vue
@@ -21,24 +21,3 @@ import FooterSection from '@/components/FooterSection.vue'
     <FooterSection />
   </div>
 </template>
-<style>
-body {
-  cursor: none;
-}
-
-/* Estilo para la imagen del cursor flotante */
-#custom-cursor {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 48px;
-  /* Ajusta el tamaño de tu GIF */
-  height: 48px;
-  pointer-events: none;
-  /* Vital para que no bloquee los clics en los botones */
-  transform: translate(-50%, -50%);
-  /* Centra la punta del cursor justo en la flecha */
-  z-index: 9999;
-  /* Asegura que siempre esté por encima de todo */
-}
-</style>
